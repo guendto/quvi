6e905c4fcf48b2929fc0f722116c32498cedd67c
Toni Gundogdu <legatvs@gmail.com>
2013-06-09 20:11:34 +0300

    status: Reuse the status line, do not use newlines
    Print all status messages to the same (status) line. Overwrite the
    previous messages.
    
    Other:
    * Add a time prefix to the printed messages
    * Tweak status messages
    
    Signed-off-by: Toni Gundogdu <legatvs@gmail.com>

--- src/status.c
+++ src/status.c
@@ -1,5 +1,5 @@
 /* quvi
- * Copyright (C) 2012  Toni Gundogdu <legatvs@gmail.com>
+ * Copyright (C) 2012,2013  Toni Gundogdu <legatvs@gmail.com>
  *
  * This file is part of quvi <http://quvi.sourceforge.net/>.
  *
@@ -24,31 +24,74 @@
 #include <quvi.h>
 
 #include "lutil.h"
+#include "sig.h"
 
 static const lutil_cb_printerr perr = lutil_print_stderr_unless_quiet;
+
+static void _saymsg(const gchar *m, const gboolean with_time)
+{
+  gsize l, i;
+  gchar *s;
+
+  if (with_time == TRUE)
+    {
+      GDateTime *n;
+      gchar *t;
+
+      n = g_date_time_new_now_local();
+      t = g_date_time_format(n, "%T");
+      g_date_time_unref(n);
+
+      if (t == NULL)
+        return;
+
+      s = g_strdup_printf("[%s] %s", t, m);
+      g_free(t);
+    }
+  else
+    s = g_strdup(m);
+
+  l = sigwinch_term_spaceleft(strlen(s));
+  perr("%s", s);
+  g_free(s);
+
+  for (i=0; i<l; ++i)
+    perr(" ");
+  perr("\r");
+}
+
+static void _say(const gchar *m)
+{
+  _saymsg(m, TRUE);
+}
+
+static void _done()
+{
+  _saymsg("", FALSE);
+}
 
 static void _fetch(const quvi_word type, const gpointer p)
 {
   if (type != QUVI_CALLBACK_STATUS_DONE)
-    perr(_("fetch <url>: "));
+    _say(_("fetch <url> ..."));
   else
-    perr(_("done\n"));
+    _done();
 }
 
 static void _resolve(const quvi_word type)
 {
   if (type != QUVI_CALLBACK_STATUS_DONE)
-    perr(_("resolve <url>: "));
+    _say(_("resolve <url> ..."));
   else
-    perr(_("done\n"));
+    _done();
 }
 
 static void _query_metainfo(const quvi_word type)
 {
   if (type != QUVI_CALLBACK_STATUS_DONE)
-    perr(_("metainfo <url>: "));
+    _say(_("metainfo <url> ..."));
   else
-    perr(_("done\n"));
+    _done();
 }
 
 QuviError cb_status(glong param, gpointer p)
